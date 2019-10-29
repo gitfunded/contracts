5ae5d9b00e56123d5711f2fdd61f5bf45367ca29
koshikraj <koshik.raj@gmail.com>
2019-10-29 23:07:59 +0530

    adds new test module for fund transfer

--- truffle-contracts/test/GitFundedGrant.test.js
+++ truffle-contracts/test/GitFundedGrant.test.js
@@ -6,7 +6,6 @@ require('chai')
 
 contract('GitFundedGrant', (accounts) => {
   let contract;
-  console.log(accounts);
 
   before(async () => {
     contract = await GitFundedGrant.deployed()
@@ -27,8 +26,9 @@ contract('GitFundedGrant', (accounts) => {
 
             await contract.addProject('001', 'testRepo1', 1000 );
             await contract.addProject('002', 'testRepo2', 2000 );
+            await contract.addProject('003', 'testRepo3', 3000 );
             let totalProject = await contract.getProjectsCount();
-            assert.equal(totalProject, 2)
+            assert.equal(totalProject, 3)
 
         });
         it('funded successfully', async () => {
@@ -40,6 +40,22 @@ contract('GitFundedGrant', (accounts) => {
             await contract.fundProject(1, {from: accounts[0], value: web3.utils.toWei("2", "ether")});
             projectInfo = await contract.fetchProject(1);
             assert.equal(projectInfo[3], web3.utils.toWei("2", "ether"));
+
+        });
+
+        it('funds transferred successfully', async () => {
+
+            await contract.fundProject(2, {from: accounts[0], value: web3.utils.toWei("1", "ether")});
+            let projectInfo = await contract.fetchProject(0);
+            assert.equal(projectInfo[3], web3.utils.toWei("1", "ether"));
+
+
+            let initBalance = await web3.eth.getBalance(accounts[1]);
+            await contract.transferFund(1, accounts[1], web3.utils.toWei("1", "ether"));
+
+            assert.equal(parseInt(initBalance) + parseInt(web3.utils.toWei("1", "ether")),
+                await web3.eth.getBalance(accounts[1]));
+
 
         });
 
